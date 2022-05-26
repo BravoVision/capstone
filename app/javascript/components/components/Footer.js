70dea601d3b358c4fad6316bdfb9fe87b6207476
zmprunella <zane.prunella@gmail.com>
2022-05-26 13:13:14 -0700

    added testing to header, footer, home, not found.

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         return (
             <>
             <Container>
-                <h1> &copy;Eventually... Brought to you by 
+                <h1>&copy;Eventually... Brought to you by 
                     <a href="https://github.com/BravoVision"> BravoVision</a>
                 </h1>  
 
